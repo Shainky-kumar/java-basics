071e38e9c9bcff9c475a63e48183ab780ea352d2
Shainky Kumar <shainkythakur1@gmail.com>
2025-04-03 22:45:46 +0530

    Add files via upload

---
+++ primitivedatatype.java
@@ -0,0 +1,54 @@
+public class primitivedatatype {
+    public static void main(String[] args) {
+        /* 
+        int a = 90;
+        int b = 67;
+        int c = a + b;
+        System.out.println(c);
+        */
+        /* 
+        float a = 3.14f;
+        int b = 8;
+        float sum = a + b ;
+System.out.println(sum);
+*/
+/* 
+int a = 10;
+int b = 4;
+//int  c = a/b;
+//double c = a/b;
+float c = a/b;
+System.out.println(c);
+*/
+int a = 12;
+int b = 18;
+if(a<b) {
+    System.out.println("adult for the vote");
+} else {
+    System.out.println(" not adult");
+}
+    }
+}
+
+
+// primitive data typee
+
+//1. integer type
+
+//a.byte= 8{bits},range(-128 to 127)
+//b.short=16{bits},range(-32768 to 32767)
+//c.int=32{bits},range(-2.1B to 2.1B)
+//d.long=64{bits},range(-4000T to 4000T)
+
+//2.floating type
+
+//a.float = (32bits),range= (3.40 x 10p38, 1.40 x 10p-45)
+//b.double = (64bits),range = (1.79 x 10p308, 4.9 x 10p-324)
+
+// character type 
+
+//a.char = (16bits),range 0 to 65536-unicode
+
+// boolean type
+
+//boolean = m/cdependent range : {true , false}
