f353639e8714150387e04ed987a6a1c6c9b890b8
Shainky Kumar <shainkythakur1@gmail.com>
2025-04-03 22:38:45 +0530

    Update basics.java

---
+++ basics.java
@@ -0,0 +1,11 @@
+
+public class basics {// class name basics
+// single line comment 
+/*
+ * this is a multiline comment 
+ * by default the java.lang packege is available to every java program
+ */
+    public static void main(String[] args) {// main is a method name , and the string is an  array where args is a name or type of this String[] array
+       System.out.println(" hello world");
+    }
+}
