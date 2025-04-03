f631c6c75487c589d76ed44203ba80ede4004544
Shainky Kumar <shainkythakur1@gmail.com>
2025-04-03 22:42:32 +0530

    Add files via upload

---
+++ basicsofjavaapnacollege.java
@@ -0,0 +1,120 @@
+// pritn the pattern
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+// pritn two line star
+// System.out.println("*");
+//  System.out.println("**");
+//  // variable 
+//  int  a = 89;
+//  int b= 78;
+//  int sum = a+b;
+//  System.out.println(sum);
+//  // subtract 
+//  int sub = a-b;
+//  System.out.println(sub);
+//  // multiply
+//  int mul = a*b;
+//  System.out.println(mul);
+//  // divide
+//  int div = a/b;
+//  System.out.println(div);
+//  // module
+//  int mod = a%b;
+//  System.out.println(mod);
+
+//  // quiz ouetion (a*b)/(a-b)
+//  int x = 10;
+//  int y = 15;
+//  int c = x*y;
+//  int d = x-y;
+//  System.out.println(c/d);
+//  //System.out.println((x*y)/(x-y));
+
+ // scanner class 
+
+//  Scanner sc = new Scanner(System.in);
+//  String name = sc.next();// for input the single line
+//  String name2 = sc.nextLine();// used to take an complete line as an input
+//  System.out.println(name);
+//  System.out.println(name2);
+
+// take an two varialbe from user input
+// Scanner sc = new Scanner(System.in);
+// int a = sc.nextInt();
+// int b = sc.nextInt();
+// int sum = a+ b;
+// System.out.println("the sum of a and b is " + sum);
+//     }
+// }
+
+
+
+
+// comditional statement 
+import java.util.Scanner;
+public class basicsofjavaapnacollege {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // vote adult prograam
+
+
+        // int age = sc.nextInt();
+        // if(age>18) {
+        //      System.out.println("eligible foe the vote");
+        // }
+        // else {
+        //     System.out.println("not eligible for the vote");
+        // }
+
+        // odd even program
+
+        //  int x = sc.nextInt();
+        //  if(x%2==0) {
+        //     System.out.println("even");
+        //  }
+        //  else {
+        //     System.out.println("odd");
+        //  }
+
+        //int a = sc.nextInt();
+        //int b = sc.nextInt();
+
+        // if(a==b) {
+        //     System.out.println("equal");
+        // } else if(a>b) {
+        //         System.out.println("a is a greater");
+        //     } else {
+        //         System.out.println("a is a leasser");
+        //     }
+
+
+        // if 3 is a even no print bazinga
+
+    // if(3%2==0) {
+    //     System.out.println("bazinga");
+    // } else {
+    //     System.out.println("not bazinga");
+    // }
+
+
+    // butoon program 
+
+    
+    int button = sc.nextInt();
+    if(button == 1) {
+        System.out.println("namaste");
+    } else if(button == 2) {
+        System.out.println("hello");
+    } else if(button == 3) {
+        System.out.println("creative");
+    }
+else {
+    System.out.println("not matched");
+}
+
+        }
+    }
