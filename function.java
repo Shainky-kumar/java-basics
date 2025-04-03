bed803cfbea8bc9c12af340edb315b0e07321663
Shainky Kumar <shainkythakur1@gmail.com>
2025-04-03 22:43:22 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,31 @@
+public class function {
+    /* 
+    public static void printName(String name){// function
+         System.out.println(name);
+return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       String name = sc.nextLine();
+       printName(name);// function calling
+    }
+}
+*/
+
+//Q.2 calculate sum of two numbers 
+/* 
+public static int calculateSum(int a, int b) {
+    int sum = a+b;
+    return sum;
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+   int a = sc.nextInt();
+   int b = sc.nextInt();
+    int sum = calculateSum(a, b);
+    System.out.println(sum);
+}
+}
+*/
+
+//Q3.
