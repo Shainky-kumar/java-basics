694a2417b5566d033c19a2a8c739d9ab9dceafeb
Shainky Kumar <shainkythakur1@gmail.com>
2025-04-03 22:43:39 +0530

    Add files via upload

---
+++ identifiers.java
@@ -0,0 +1,64 @@
+public class identifiers {
+    public static void main(String[] args) {
+        //String first name = " shainky "// no white space is allow
+        String firstname = " shainky";
+        final double INTEREST_RATE = 8.9;// constant not be changed 
+        System.out.println("hello world");
+  
+
+//1. there are 53 keyword in java
+//2. and 8 data type
+//3 . these are all the identifiers
+// public-keyword
+//class-keyword
+//class name-name of a class or user defined
+//static- keyword
+//void-keyword
+// main-name of a method- or user defined
+//string- name of a class - API class- java.lang.String-predefined 
+//args-variable of type String
+//system-name of a class - java.lang.system- predefined class
+//out-variable-system-static
+//println-name of a method- predefined method-printStream
+//hello java-literl or string literal
+
+
+
+
+// variable = hold a data iteam of some type
+// String[] args
+// int x;// whole
+//double age;// number
+//boolean status;//{true,false}
+//data typ:identifier(keyword)
+
+
+// variable declaration
+int age;
+String name;
+boolean status;
+
+//initialiazation 
+int a = 1;
+float b = 3.14f;
+double salary = 6789;
+
+// variable assignment
+age = 90;
+status = false;
+
+
+
+
+// constant
+
+//1. final 
+}
+}
+
+
+
+
+
+
+
